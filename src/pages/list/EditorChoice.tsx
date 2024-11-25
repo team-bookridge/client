0714529d2d01b699a0ea7fd0022d2f1d2bf3312f
ogg1996 <rjsrnr43@naver.com>
2024-11-25 14:32:19 +0900

    #10 feat/리스트(베스트셀러, 신간, 편집자추천) 기본 프레임 추가

--- src/pages/list/EditorChoice.tsx
+++ src/pages/list/EditorChoice.tsx
@@ -1,5 +1,118 @@
+import { useState } from 'react';
+
+type categoryItem = {
+  id: number;
+  name: string;
+};
 function EditorChoice() {
-  return <div className="h-[1000px]">편집자 추천</div>;
+  const [categoryChoice, setcategoryChoice] = useState<number>(170);
+
+  const category: categoryItem[] = [
+    {
+      id: 170,
+      name: '경제/경영',
+    },
+    {
+      id: 2105,
+      name: '고전',
+    },
+    {
+      id: 987,
+      name: '과학',
+    },
+    {
+      id: 2551,
+      name: '만화',
+    },
+    {
+      id: 1,
+      name: '소설/시/희곡',
+    },
+    {
+      id: 55889,
+      name: '에세이',
+    },
+    {
+      id: 74,
+      name: '역사',
+    },
+    {
+      id: 517,
+      name: '예술/대중문화',
+    },
+    {
+      id: 1322,
+      name: '외국어',
+    },
+    {
+      id: 656,
+      name: '인문학',
+    },
+    {
+      id: 336,
+      name: '자기계발',
+    },
+    {
+      id: 112011,
+      name: '장르소설',
+    },
+    {
+      id: 351,
+      name: '컴퓨터/모바일',
+    },
+    {
+      id: 55890,
+      name: '건강/취미/레저',
+    },
+    {
+      id: 2913,
+      name: '잡지',
+    },
+  ];
+  return (
+    <>
+      <h2 className="text-[1.5rem] border-b-2">편집자추천</h2>
+      <div className="flex flex-wrap gap-[0.5rem] p-[0.5rem] border-2 rounded-[0.5rem]">
+        {category.map((el: categoryItem) => (
+          <button
+            type="button"
+            key={el.id}
+            className={`p-[0.25rem] border-2 rounded-[0.5rem] ${el.id === categoryChoice && 'bg-gray-300'}`}
+            onClick={() => {
+              setcategoryChoice(el.id);
+            }}>
+            {el.name}
+          </button>
+        ))}
+      </div>
+      <div className="flex flex-wrap">
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item1
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item2
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item3
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item4
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item5
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item6
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item7
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item8
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default EditorChoice;
