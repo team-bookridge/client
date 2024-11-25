d6e49f9fd806f5d28504043927971a9519b139fc
ogg1996 <rjsrnr43@naver.com>
2024-11-26 00:36:18 +0900

    #15 feat/모달 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import '@/App.css';
+import { TModal } from '@/types';
 import Footer from '@components/app/Footer';
 import Header from '@components/app/Header';
+import HeaderMenuModal from '@components/modal/HeaderMenuModal';
+import LoginModal from '@components/modal/LoginModal';
+import SetNickNameModal from '@components/modal/SetNickNameModal';
 import BookDetail from '@pages/BookDetail';
 import Home from '@pages/Home';
 import BestSeller from '@pages/list/BestSeller';
@@ -9,13 +13,27 @@ import NewBook from '@pages/list/NewBook';
 import MyPage from '@pages/MyPage';
 import Search from '@pages/Search';
 import WishList from '@pages/WishList';
+import { useState } from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [modal, setModal] = useState<TModal>('none');
   return (
     <>
-      <Header />
+      {modal !== 'none' && (
+        <div
+          className="fixed w-full h-full bg-black/50 z-30 flex justify-center"
+          onClick={() => {
+            setModal('none');
+          }}
+          aria-hidden="true">
+          {modal === 'login' && <LoginModal />}
+          {modal === 'setNickName' && <SetNickNameModal />}
+          {modal === 'HeaderMenu' && <HeaderMenuModal />}
+        </div>
+      )}
+      <Header setModal={setModal} />
       <div className="flex flex-col w-[64rem] gap-6 px-[1.25rem] pt-[5rem]">
         <Routes>
           <Route path="/" element={<Home />} />
