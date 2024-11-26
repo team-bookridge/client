5f4e4942447d1a102e60d49d3198410451d87563
ogg1996 <rjsrnr43@naver.com>
2024-11-26 23:10:23 +0900

    #17 feat/커스텀 훅 임시 사용 코드 작성(추후 수정)

--- src/pages/list/BestSeller.tsx
+++ src/pages/list/BestSeller.tsx
@@ -1,33 +1,43 @@
+import useInfiniteGetData from '@/hooks/list/useInfiniteGetListData';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
 function BestSeller() {
+  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteGetData(
+      'Bestseller-List',
+      import.meta.env.VITE_ALADIN_API_URL,
+      import.meta.env.VITE_ALADIN_API_KEY,
+      'Bestseller'
+    );
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+
   return (
     <>
       <h2 className="text-[1.5rem] border-b-2">베스트셀러</h2>
       <div className="flex flex-wrap">
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item1
-        </div>
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item2
-        </div>
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item3
-        </div>
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item4
-        </div>
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item5
-        </div>
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item6
-        </div>
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item7
-        </div>
-        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
-          item8
-        </div>
+        {!isLoading &&
+          data?.pages.map((page) =>
+            page.item.map(
+              (el: { isbn13: number; cover: string; title: string }) => (
+                <div
+                  key={el.isbn13}
+                  className="md:w-[50%] w-[100%] h-[300px] border-b-2">
+                  <img src={el.cover} alt={el.title} />
+                </div>
+              )
+            )
+          )}
       </div>
+      <h1 ref={ref}>Load more</h1>
     </>
   );
 }
