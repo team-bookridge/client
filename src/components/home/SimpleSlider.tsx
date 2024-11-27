4b5873b5a0e8cb802e556b43ac68c43c94755357
mac <chanhee0708jdm@gmail.com>
2024-11-27 13:26:48 +0900

    #11 feat/ React-Slick 기능 개

---
+++ src/components/home/SimpleSlider.tsx
@@ -0,0 +1,65 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function SimpleSlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings} centerMode={true}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+}
+
+export default SimpleSlider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,50 @@
-function Home() {
-  return <div className="h-[1000px]">Home</div>;
-}
+import React from 'react';
+import SimpleSlider from '../components/home/SimpleSlider';
+import Responsive from '../components/home/Responsive';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container">
+      {/* SimpleSlider 섹션 */}
+      <div className="ad-container">
+        <SimpleSlider />
+      </div>
+
+      {/* 베스트 셀러 섹션 */}
+      <div className="book-list-container">
+        <h2>베스트 셀러</h2>
+        <Responsive />
+      </div>
+
+      {/* 주목할 만한 신간 섹션 */}
+      <div className="book-list-container">
+        <h2>주목할 만한 신간</h2>
+        <Responsive />
+      </div>
+
+      {/* 편집자 추천 섹션 */}
+      <div className="category-container">
+        <h2>편집자 추천</h2>
+        <div className="categories">
+          {/* 카테고리 목록 */}
+          <div className="category-card">
+            <h3>카테고리 이름</h3>
+            <p>카테고리 설명</p>
+            <button type="button">자세히 보기</button>
+          </div>
+          {/* 더 많은 카테고리 카드 추가 */}
+        </div>
+        <button type="button" className="more-button">
+          더 보기
+        </button>
+      </div>
+
+      {/* 추가 책 목록 섹션 */}
+      <div className="book-list-container">
+        <Responsive />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
