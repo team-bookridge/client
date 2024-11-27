7833f672d970a955d7c250bc06fb25d0d2e4957f
ogg1996 <rjsrnr43@naver.com>
2024-11-27 10:59:40 +0900

    작업 사항 임시 커밋

--- src/pages/list/BestSeller.tsx
+++ src/pages/list/BestSeller.tsx
@@ -11,6 +11,8 @@ function BestSeller() {
       'Bestseller'
     );
 
+  console.log(data);
+
   const { ref, inView } = useInView();
 
   useEffect(() => {
@@ -21,7 +23,7 @@ function BestSeller() {
   }, [inView]);
 
   return (
-    <>
+    <div className="flex flex-col">
       <h2 className="text-[1.5rem] border-b-2">베스트셀러</h2>
       <div className="flex flex-wrap">
         {!isLoading &&
@@ -30,15 +32,46 @@ function BestSeller() {
               (el: { isbn13: number; cover: string; title: string }) => (
                 <div
                   key={el.isbn13}
-                  className="md:w-[50%] w-[100%] h-[300px] border-b-2">
-                  <img src={el.cover} alt={el.title} />
+                  className="md:w-[50%] flex flex-col gap-[1rem] py-[1rem] w-[100%] border-b-2">
+                  <div className="flex">
+                    <img
+                      className="w-[9rem] max-h-[13rem]"
+                      src={el.cover}
+                      alt={el.title}
+                    />
+                    <div className="flex flex-col flex-grow pr-[0.5rem]">
+                      <p>책이름</p>
+                      <p>저자</p>
+                      <p>출판사</p>
+                      <p>출판일</p>
+                      <p>가격</p>
+                      <p>설명</p>
+                    </div>
+                  </div>
+                  <div className="flex justify-around">
+                    <button
+                      className="text-[1.5rem] bg-gray-300 "
+                      type="button">
+                      알라딘
+                    </button>
+                    <button
+                      className="text-[1.5rem] bg-gray-300 "
+                      type="button">
+                      교보문고
+                    </button>
+                    <button
+                      className="text-[1.5rem] bg-gray-300 "
+                      type="button">
+                      예스24
+                    </button>
+                  </div>
                 </div>
               )
             )
           )}
       </div>
       <h1 ref={ref}>Load more</h1>
-    </>
+    </div>
   );
 }
 
