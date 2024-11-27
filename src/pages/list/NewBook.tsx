90c0d03fa1f02b76571d2ab938aba79fda7ae8a3
mac <chanhee0708jdm@gmail.com>
2024-11-27 14:20:00 +0900

    병합완료

--- src/pages/list/NewBook.tsx
+++ src/pages/list/NewBook.tsx
@@ -1,5 +1,35 @@
 function NewBook() {
-  return <div className="h-[1000px]">신간도서</div>;
+  return (
+    <>
+      <h2 className="text-[1.5rem] border-b-2">신간도서</h2>
+      <div className="flex flex-wrap">
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item1
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item2
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item3
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item4
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item5
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item6
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item7
+        </div>
+        <div className="md:w-[50%] bg-gray-400 w-[100%] h-[300px] border-b-2">
+          item8
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NewBook;
