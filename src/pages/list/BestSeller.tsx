a43f7f423daf8fec5f9d407083e5c189ecfe8e56
ogg1996 <rjsrnr43@naver.com>
2024-11-29 11:34:57 +0900

    #21 feat/리스트 페이지(베스트셀러, 신간도서, 편집자 추천) 작업

--- src/pages/list/BestSeller.tsx
+++ src/pages/list/BestSeller.tsx
@@ -1,4 +1,6 @@
 import useInfiniteGetData from '@/hooks/list/useInfiniteGetListData';
+import { TResponseBookItemInfo } from '@/types';
+import BookItem from '@components/common/BookItem';
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
@@ -11,8 +13,6 @@ function BestSeller() {
       'Bestseller'
     );
 
-  console.log(data);
-
   const { ref, inView } = useInView();
 
   useEffect(() => {
@@ -24,50 +24,15 @@ function BestSeller() {
 
   return (
     <div className="flex flex-col">
-      <h2 className="text-[1.5rem] border-b-2">베스트셀러</h2>
+      <h2 className="text-[1.5rem] py-[1rem] text-[#4F772D;] border-b-4 border-[#C0CFB2] font-[900]">
+        베스트셀러
+      </h2>
       <div className="flex flex-wrap">
         {!isLoading &&
           data?.pages.map((page) =>
-            page.item.map(
-              (el: { isbn13: number; cover: string; title: string }) => (
-                <div
-                  key={el.isbn13}
-                  className="md:w-[50%] flex flex-col gap-[1rem] py-[1rem] w-[100%] border-b-2">
-                  <div className="flex">
-                    <img
-                      className="w-[9rem] max-h-[13rem]"
-                      src={el.cover}
-                      alt={el.title}
-                    />
-                    <div className="flex flex-col flex-grow pr-[0.5rem]">
-                      <p>책이름</p>
-                      <p>저자</p>
-                      <p>출판사</p>
-                      <p>출판일</p>
-                      <p>가격</p>
-                      <p>설명</p>
-                    </div>
-                  </div>
-                  <div className="flex justify-around">
-                    <button
-                      className="text-[1.5rem] bg-gray-300 "
-                      type="button">
-                      알라딘
-                    </button>
-                    <button
-                      className="text-[1.5rem] bg-gray-300 "
-                      type="button">
-                      교보문고
-                    </button>
-                    <button
-                      className="text-[1.5rem] bg-gray-300 "
-                      type="button">
-                      예스24
-                    </button>
-                  </div>
-                </div>
-              )
-            )
+            page.item.map((el: TResponseBookItemInfo) => (
+              <BookItem key={el.itemId} bookInfo={el} />
+            ))
           )}
       </div>
       <h1 ref={ref}>Load more</h1>
