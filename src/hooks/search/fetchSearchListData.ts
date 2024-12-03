33abc3b7375f4b0a3de258ae91cea2374a5f9084
ogg1996 <rjsrnr43@naver.com>
2024-12-04 01:55:23 +0900

    #37 feat/모달 전용 커스텀 훅 추가, 받아오는 데이터 수가 다르므로 데이터 패칭 코드에서 파람 추가(maxResults), 파람 추가로 인한 코드 변경

--- src/hooks/search/fetchSearchListData.ts
+++ src/hooks/search/fetchSearchListData.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const fetchSearchListData = async (query: string, page: number = 1) => {
+const fetchSearchListData = async (
+  query: string,
+  page: number = 1,
+  maxResults: number = 5
+) => {
   const response = await axios.get(
     `${import.meta.env.VITE_PROXY_OPEN_API_URL}/aladin/search`,
     {
@@ -8,6 +12,7 @@ const fetchSearchListData = async (query: string, page: number = 1) => {
         ttbKey: import.meta.env.VITE_ALADIN_API_KEY,
         Query: query,
         QueryType: 'Keyword',
+        MaxResults: String(maxResults),
         Start: String(page),
         Sort: 'Accuracy',
         SearchTarget: 'Book',
