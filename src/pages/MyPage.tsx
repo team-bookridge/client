81dc877aeee82d557451245de708c8e5b1602bd4
ogg1996 <rjsrnr43@naver.com>
2024-12-09 11:16:19 +0900

    #54 feat/코드 간소화, 기능 연결

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,85 +1,65 @@
-import { useEffect, useState } from 'react';
+import useAccessCheck from '@/hooks/useAccessCheck';
+import useAuthStore from '@/stores/authStore';
+import useModalStore from '@/stores/modalStore';
+import { deleteUserData } from '@/supabase';
+import scrollToTop from '@/utils/scrollToTop';
 import { Link } from 'react-router-dom';
-import type { Dispatch, SetStateAction } from 'react';
-import { TModal } from '@/types';
 
-type MyPageProps = {
-  setModal: Dispatch<SetStateAction<TModal>>;
-};
+function MyPage() {
+  scrollToTop();
 
-type ProfileProps = {
-  name: string;
-  emailOrId: string;
-};
+  const { setModal } = useModalStore();
+  const { profile, wishList } = useAuthStore();
 
-type InfoButtonProps = {
-  label: string;
-  onClick: () => void;
-};
+  const isAllowAccess = useAccessCheck('잘못된 접근입니다!');
 
-function Profile({ name, emailOrId }: ProfileProps): JSX.Element {
-  return (
-    <div className="flex items-center gap-4">
-      <div className="w-[6rem] h-[6rem] md:w-[8rem] md:h-[8rem] bg-[#C0CFB2] rounded-full" />
-      <div>
-        <p className="font-bold text-lg text-center md:text-left">{name}</p>
-        <p className="text-sm text-gray-600 text-center md:text-left">
-          {emailOrId}
-        </p>
-      </div>
-    </div>
-  );
-}
-
-function InfoButton({ label, onClick }: InfoButtonProps): JSX.Element {
-  return (
-    <button
-      type="button"
-      onClick={onClick}
-      className="text-white bg-[#4F772D] w-[8rem] py-3 rounded hover:bg-[#3b5e23] text-center">
-      {label}
-    </button>
-  );
-}
-
-function MyPage({ setModal }: MyPageProps): JSX.Element {
-  const [wishListCount, setWishListCount] = useState<number>(0);
-
-  useEffect(() => {
-    const storedWishList = JSON.parse(localStorage.getItem('wishList') || '[]');
-    setWishListCount(storedWishList.length);
-  }, []);
+  if (!isAllowAccess) return null;
 
   return (
     <div className="flex flex-col items-center min-h-[950px] w-full mx-auto gap-6">
-      {/* 페이지 헤더 */}
       <h2 className="text-[1.5rem] font-bold text-[#4F772D] border-b-4 border-[#C0CFB2] py-[1rem] w-full text-left">
         마이페이지
       </h2>
-      {/* 마이페이지 전체 박스 */}
       <div className="flex flex-col w-full max-w-[40rem] p-6 rounded-lg border border-[#4F772D]">
         <div className="flex flex-col md:flex-row items-center justify-between w-full gap-4">
-          {/* 프로필 섹션 */}
-          <Profile name="홍길동님" emailOrId="아이디(기본) or 이메일" />
-          {/* 버튼 섹션 */}
+          <div className="flex items-center gap-4">
+            <img
+              className="w-[6rem] h-[6rem] md:w-[8rem] md:h-[8rem] rounded-full"
+              src={profile?.avatar_url}
+              alt="유저 프로필 이미지"
+            />
+            <div>
+              <p className="font-bold text-lg text-center md:text-left">
+                {profile?.nickname}
+              </p>
+              <p className="text-sm text-gray-600 text-center md:text-left">
+                {profile?.email}
+              </p>
+            </div>
+          </div>
           <div className="flex flex-row md:flex-col gap-2 items-center md:items-end justify-center w-full md:w-auto">
-            <InfoButton
-              label="닉네임 변경"
+            <button
+              type="button"
               onClick={() => setModal('setNickName')}
-            />
-            <InfoButton
-              label="회원 탈퇴"
-              onClick={() => alert('정말 탈퇴하시겠습니까?')}
-            />
+              className="text-white bg-[#4F772D] w-[8rem] py-3 rounded hover:bg-[#3b5e23] text-center">
+              닉네임 변경
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                if (profile) deleteUserData(profile.id);
+              }}
+              className="text-white bg-[#4F772D] w-[8rem] py-3 rounded hover:bg-[#3b5e23] text-center">
+              회원 탈퇴
+            </button>
           </div>
         </div>
       </div>
-      {/* 찜 목록 */}
       <Link
         to="/MyPage/WishList"
         className="w-full max-w-[40rem] border border-[#4F772D] p-4 rounded-lg shadow-sm hover:shadow-md transition flex flex-col items-start">
         <p className="font-bold text-lg text-left">찜 목록</p>
-        <p className="text-sm text-gray-600 text-left">{wishListCount}개</p>
+        <p className="text-sm text-gray-600 text-left">{wishList.length}개</p>
       </Link>
     </div>
   );
