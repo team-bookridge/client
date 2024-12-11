cad7ee9eb3353a3d40645fafe007ceea08709fab
ogg1996 <rjsrnr43@naver.com>
2024-12-11 10:42:35 +0900

    #74 feat/리뷰 기능 연결 작업 및 디자인

--- src/components/home/ReviewList.tsx
+++ src/components/home/ReviewList.tsx
@@ -1,31 +1,32 @@
-interface Review {
-  id: string;
-  text: string;
+interface Props {
+  data:
+    | {
+        content_id: string;
+        updated_at: string;
+        user_nickname: string;
+        review: string;
+      }[]
+    | undefined;
 }
 
-interface ReviewListProps {
-  reviews: Review[];
-  onDeleteReview: (id: string) => void;
-}
-
-function ReviewList({ reviews, onDeleteReview }: ReviewListProps): JSX.Element {
+function ReviewList({ data }: Props) {
   return (
-    <div className="mt-6 bg-white rounded-lg shadow-md p-4">
+    <div className="mt-6 bg-white rounded-lg">
       <h2 className="text-base sm:text-lg font-semibold mb-4">리뷰</h2>
-      {reviews.length === 0 ? (
-        <p>작성된 리뷰가 없습니다.</p>
+      {!data ? (
+        <div>등록된 리뷰가 없습니다.</div>
       ) : (
-        reviews.map((review) => (
+        data?.map((item) => (
           <div
-            key={review.id}
-            className="border-b py-2 flex justify-between items-center">
-            <p className="text-sm sm:text-base text-gray-700">{review.text}</p>
-            <button
-              type="button"
-              onClick={() => onDeleteReview(review.id)}
-              className="text-red-500 ml-4 hover:underline">
-              삭제
-            </button>
+            key={`${item.content_id}-${item.updated_at}`}
+            className="border-b py-2 flex flex-col rounded-lg shadow-md p-4 gap-[1rem]">
+            <div className="flex justify-between text-[1.125rem] font-[900]">
+              <p>{item.user_nickname}</p>
+              <p>{item.updated_at}</p>
+            </div>
+            <p className="text-sm sm:text-base text-gray-700 whitespace-pre-line">
+              {item.review}
+            </p>
           </div>
         ))
       )}
