d34c436e563687fcd38c863719fcc947f0773567
ogg1996 <rjsrnr43@naver.com>
2024-12-11 10:34:21 +0900

    #74 feat/리뷰 슈파베이스 메서드 작업, 스위트 알럿을 사용한  알림창 추가 작업

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,6 @@
 import { TAuthProvider, TWishItem } from '@/types';
 import { createClient } from '@supabase/supabase-js';
+import Swal from 'sweetalert2';
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_PROJECT_URL,
@@ -35,10 +36,15 @@ export const setUserNickname = async (userId: string, newNickname: string) => {
 };
 
 export const deleteUserData = async (userId: string) => {
-  const confirmed = window.confirm(
-    '정말로 회원 탈퇴를 진행하시겠습니까?\n(탈퇴하면 회원님의 찜목록, 리뷰 목록이 전부 사라집니다!)'
-  );
-  if (!confirmed) return;
+  const { isConfirmed } = await Swal.fire({
+    icon: 'warning',
+    title: '회원탈퇴',
+    text: '회원님의 정보가 전부 삭제됩니다',
+    showDenyButton: true,
+    confirmButtonText: '탈퇴하기',
+    denyButtonText: `취소`,
+  });
+  if (!isConfirmed) return;
 
   const { error: deleteUserArror } =
     await supabase.auth.admin.deleteUser(userId);
@@ -103,4 +109,84 @@ export const deleteSelectedWishItems = async (
     return false;
   }
   return true;
+};
+
+export const addReview = async (
+  userId: string,
+  userNickname: string,
+  contentId: string,
+  review: string
+) => {
+  const { error } = await supabase.from('reviewlist').insert([
+    {
+      user_id: userId,
+      user_nickname: userNickname,
+      content_id: contentId,
+      review,
+    },
+  ]);
+  if (error) return false;
+  return true;
+};
+
+export const updateReview = async (
+  userId: string,
+  contentId: string,
+  review: string
+) => {
+  const { isConfirmed } = await Swal.fire({
+    icon: 'question',
+    text: '리뷰를 수정하시겠습니까?',
+    showDenyButton: true,
+    confirmButtonText: '수정하기',
+    denyButtonText: `취소`,
+  });
+  if (!isConfirmed) return false;
+  const { error } = await supabase
+    .from('reviewlist')
+    .update([
+      {
+        review,
+        updated_at: new Date(),
+      },
+    ])
+    .eq('user_id', userId)
+    .eq('content_id', contentId);
+
+  if (error) return false;
+  return true;
+};
+
+export const deleteReview = async (userId: string, contentId: string) => {
+  const { isConfirmed } = await Swal.fire({
+    icon: 'question',
+    text: '리뷰를 삭제하시겠습니까?',
+    showDenyButton: true,
+    confirmButtonText: '삭제하기',
+    denyButtonText: `취소`,
+  });
+  if (!isConfirmed) return false;
+
+  const { error } = await supabase
+    .from('reviewlist')
+    .delete()
+    .eq('user_id', userId)
+    .eq('content_id', contentId);
+
+  if (error) return false;
+  return true;
+};
+
+export const getReviewsByContent = async (contentId: string) => {
+  const { data, error } = await supabase
+    .from('reviewlist')
+    .select('content_id, user_nickname, review, updated_at')
+    .order('updated_at', { ascending: false })
+    .eq('content_id', contentId);
+
+  if (error) {
+    return [];
+  }
+
+  return data;
 };
